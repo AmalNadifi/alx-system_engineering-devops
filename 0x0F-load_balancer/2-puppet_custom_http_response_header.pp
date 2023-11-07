4f33bc0ad09b6679396093ad6896111440f339da
AmalNadifi <amal.nadifi9@gmail.com>
2023-11-07 10:17:54 -0800

    task2 modified

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,17 +1,47 @@
-# Automation of the task : creating a custom HTTP header response with Puppet
-# Automation: Creating a custom HTTP header response with Puppet
+# Automating the task: Creating a custom HTTP header response with Puppet
 
-exec { 'command':
-  # Update the package list
-  command => 'apt-get -y update &&
+# Step 1: Update the package list
+exec { 'update':
+  command => 'apt-get -y update',
+  path    => '/usr/bin',
+  creates => '/var/lib/apt/periodic/update-success-stamp',
+}
 
-  # Install Nginx
-  apt-get -y install nginx &&
+# Step 2: Install Nginx
+exec { 'install':
+  command => 'apt-get -y install nginx',
+  path    => '/usr/bin',
+  require => Exec['update'],
+}
 
-  # Adding a custom HTTP header 'X-Served-By' with the server's hostname to the Nginx configuration
-  sed -i "/listen 80 default_server;/a add_header X-Served-By $::hostname;" /etc/nginx/sites-available/default &&
+# Step 3: Configure Nginx to add a custom HTTP header X-Served-By with the servers hostname
+file { '/etc/nginx/sites-available/custom_header':
+  ensure  => present,
+  content => "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    server_name _;
+    add_header X-Served-By $hostname;
+  }",
+  require => Exec['install'],
+}
 
-  # Restarting the Nginx service to apply the changes
-  service nginx restart',
-  provider => shell,
+# Step 4: Create a symbolic link to enable the custom configuration
+file { '/etc/nginx/sites-enabled/custom_header':
+  ensure => link,
+  target => '/etc/nginx/sites-available/custom_header',
+  require => File['/etc/nginx/sites-available/custom_header'],
+}
+
+# Step 5: Remove the default Nginx site configuration
+file { '/etc/nginx/sites-enabled/default':
+  ensure => absent,
+  require => File['/etc/nginx/sites-available/custom_header'],
+}
+
+# Step 6: Restart the Nginx service to apply the changes
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  require   => [File['/etc/nginx/sites-enabled/custom_header'], File['/etc/nginx/sites-available/custom_header']],
 }
