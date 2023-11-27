6ad09a8df351557332faa3d1c0d8a0228c1a7d00
AmalNadifi <amal.nadifi9@gmail.com>
2023-11-27 08:47:07 -0800

    corrected the code to give the required outputs

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,45 +5,30 @@ import requests
 import sys
 
 
-def fetch_user_info(employee_id):
-    """Fetch user information based on employee_id."""
-    url = "https://jsonplaceholder.typicode.com/"
-    user_response = requests.get(url + "users/{}".format(employee_id))
-    return user_response.json()
-
-
-def fetch_todo_list(employee_id):
-    """Fetch TODO list for a given employee_id."""
-    url = "https://jsonplaceholder.typicode.com/"
-    todo_response = requests.get(url + "todos", params={"userId": employee_id})
-    return todo_response.json()
-
-
-def export_to_csv(employee_id, user_name, tasks):
-    """Export tasks to a CSV file."""
-    filename = "{}.csv".format(employee_id)
-    with open(filename, mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow([
-            "USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-            )
-        for task in tasks:
-            writer.writerow([
-                employee_id, user_name, str(task['completed']), task['title']]
-                )
-    print("Data exported to {}".format(filename))
-
-
 if __name__ == "__main__":
+    # Check for the correct number of command-line arguments
     if len(sys.argv) != 2 or not sys.argv[1].isdigit():
-        print("Usage: python3 1-export_to_CSV.py <employee_id>")
+        print("Usage: python3 script.py <employee_id>")
     else:
+        # Extract employee ID from command-line argument
         employee_id = sys.argv[1]
 
-        # Fetch user info & Todo list using gather_data_from_an_API.py func
-        user_data = fetch_user_info(employee_id)
-        user_name = user_data.get("name")
-        todo_data = fetch_todo_list(employee_id)
+        # API base URL
+        base_url = "https://jsonplaceholder.typicode.com/"
+
+        # Fetch user information
+        user_response = requests.get(
+                base_url + "users/{}".format(employee_id)).json()
+        employee_username = user_response.get("username")
+
+        # Fetch Todo list
+        todos_response = requests.get(
+                base_url + "todos", params={"userId": employee_id}).json()
 
         # Export to CSV
-        export_to_csv(employee_id, user_name, todo_data)
+        with open("{}.csv".format(employee_id), "w", newline="") as csvfile:
+            csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+            [csv_writer.writerow(
+                [employee_id, employee_username,
+                    todo.get("completed"), todo.get("title")]
+             ) for todo in todos_response]
