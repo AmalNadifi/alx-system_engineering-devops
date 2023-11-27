7f62e836e38a851137269a0845c5b439ecbf4ffb
AmalNadifi <amal.nadifi9@gmail.com>
2023-11-27 09:46:47 -0800

    pep8 fixed again

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,15 +7,19 @@ import json
 import requests
 
 if __name__ == "__main__":
+    # Retrieving user data from the API
     url = "https://jsonplaceholder.typicode.com/"
     users = requests.get(url + "users").json()
 
     with open("todo_all_employees.json", "w") as jsonfile:
+        # Creating a JSON structure with user IDs as keys
+        # & associated tasks as values
         json.dump({
             user.get("id"): [{
-                "task": todo.get("title"),
-                "completed": todo.get("completed"),
+                "task": t.get("title"),
+                "completed": t.get("completed"),
                 "username": user.get("username")
-            } for td in requests.get(url + "todos_reponse",
-                                     params={"userId": user.get("id")}).json()
-            ] for user in users}, jsonfile)
+            } for t in requests.get(url + "todos",
+                                    params={"userId": user.get("id")}).json()
+            ] for user in users
+        }, jsonfile)
