96c4a99c4f7a423e26a6d700a5aec4d7cbad5e64
AmalNadifi <amal.nadifi9@gmail.com>
2023-12-12 10:14:33 -0800

    task solved

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,8 @@
+# This is a puppet script to automate the issue fix of Apache returning a 500 error
+
+$file_to_edit = '/var/www/html/wp-settings.php'
+
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
+}
